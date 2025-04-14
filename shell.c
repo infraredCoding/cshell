1cfa22fb1d9a7d85d53c13fa548caccbfd1bf74d
Imran Rahman <imranrahman.ix@gmail.com>
2025-04-14 03:18:18 +0000

    suport piping

--- shell.c
+++ shell.c
@@ -7,6 +7,8 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
+
+
 void parse_input_tokens(char* input, char** args, char** input_file, char** output_file, int* append){
   char* token = strtok(input, " \t\n");
   int i = 0;
@@ -37,6 +39,68 @@ void parse_input_tokens(char* input, char** args, char** input_file, char** outp
   args[i] = NULL;
 }
 
+void execute_piped_commands(char* input_line){
+  char* commands[20];
+  int num_cmds = 0;
+  char* token = strtok(input_line, "|");
+
+  while(token != NULL && num_cmds < 20){
+    commands[num_cmds++] = token;
+    token = strtok(NULL, "|");
+  }
+
+  int pipe_fds[2* (num_cmds - 1)];
+
+  for (int i=0; i< num_cmds - 1; i++){
+    if(pipe(pipe_fds + i*2) < 0){
+      perror("pipe error");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  for (int i=0; i< num_cmds; i++) {
+    pid_t pid = fork();
+    if (pid == 0){
+      char* args[100], *input_file = NULL, *output_file = NULL;
+      int append = 0;
+
+      parse_input_tokens(commands[i], args, &input_file, &output_file, &append);
+
+      if (i > 0){
+        // not the first command. pipe can be redirected
+        dup2(pipe_fds[(i-1)*2], STDIN_FILENO);
+      }else if (input_file) {
+        int fd = open(input_file, O_RDONLY);
+        dup2(fd, STDIN_FILENO);
+        close(fd);
+      }
+
+      if (i < num_cmds - 1) {
+        // not the last one. pass the output to next
+        dup2(pipe_fds[i*2 + 1], STDOUT_FILENO);
+      }else if(output_file) {
+        int fd = open(output_file, O_WRONLY | O_CREAT | (append ? O_APPEND : O_TRUNC), 0644);
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
+      }
+
+      for (int j=0; j < 2 * (num_cmds -1); j++) {
+        close(pipe_fds[j]);
+      }
+
+      execvp(args[0], args);
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  for (int i=0; i< 2 * (num_cmds - 1); i++) {
+    close(pipe_fds[i]);
+  }
+
+  for (int i = 0; i < num_cmds; i++) {
+    wait(NULL);
+  }
+}
 
 void execute_command(char** args, char* inp_file, char* out_file, int append){
 
@@ -89,9 +153,8 @@ int main() {
       break;
     }
 
-    parse_input_tokens(buffer, args, &input_file, &output_file, &append);
-    execute_command(args, input_file, output_file, append);
-    //printf("%s", buffer);
+    execute_piped_commands(buffer);
+
   }
   return 0;
 }
